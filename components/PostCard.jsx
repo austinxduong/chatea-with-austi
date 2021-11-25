b88b756d2fdfffb6314ca34bc1ac816c6053e4a4
austinxduong <axduong2@gmail.com>
2021-11-25 11:52:16 -0800

    wire components and add categories + postwidget

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
     return (
         <div>
-            
-        </div>
+            {post.title}
+            {post.excerpt}
+        </div> 
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components';
 
 const posts = [
   { title: 'First Post', excerpt: 'testingtestingtesting its almost thanksgiving' },
@@ -12,18 +13,19 @@ export default function Home() {
         <title>austinxduong</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid grid-cols-1 lg:grid-cols-5 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <div> 
-              {post.title}
-              {post.excerpt}
-            </div>
+              <PostCard
+              post={post}
+              key={post.title}
+              />
           ))}
           </div>
           <div className="lg:col-span-4 col-span-1">
               <div className="lg:sticky relative top-8">
-                
+                <PostWidget />
+                <Categories />
               </div>
           </div>
         </div>
