5588a383d168112445365d26483e0c1669e8f537
austinxduong <axduong2@gmail.com>
2021-11-29 17:53:17 -0800

    interestinggg

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const PostCard = ({ post }) => {
     return (
-        <div>
-            {post.title}
-            {post.excerpt}
+        <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+            <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+                <img
+                    src={post.featuredImage.url}
+                    alt={post.title}
+                />
+            </div>
+
         </div> 
     )
 }
