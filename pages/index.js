16fbf45e3cf2c640cf724fb4f5f64ed7e7b18d98
austinxduong <axduong2@gmail.com>
2021-11-30 14:58:19 -0800

    dreamin dreamin

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home ({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8 opacity-96">
       <Head>
-        <title>austinxduong</title>
+        <title>austinxduong ☁️✨</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
