695dfced60be72a79f9b7424ed03e11d80b6646e
austinxduong <axduong2@gmail.com>
2021-12-01 02:02:23 -0800

    good enough

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'outfit': ['Outfit']
+    },
     extend: {
-      fontFamily: {
-        'outfit': ['Outfit']
-      }
+      colors: {
+        fuchsia: '#F181F1'
+        },
+
     },
   },
   variants: {
