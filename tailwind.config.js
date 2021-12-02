bf1e91716c7d5c4055d731e516100180a0cdb0b8
austinxduong <axduong2@gmail.com>
2021-12-01 18:26:12 -0800

    font testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      'outfit': ['Outfit']
-    },
     extend: {
-      colors: {
-        fuchsia: '#F181F1'
-        },
-
+      fontFamily: {
+        'outfit': ['Outfit']
+      },
     },
   },
   variants: {
