0e20c4ff94dbb09e4639d9e2dc7107c15193aab3
austinxduong <axduong2@gmail.com>
2021-12-01 23:58:03 -0800

    create getCategories function, and declare state

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import Link from 'react';
+
 
 const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
     return (
         <div className="bg-white shadow-lg p-3 mb-8 transition duration-500 ease transform hover:-translate-y-1">
             Categories placeholder
--- services/index.js
+++ services/index.js
@@ -78,3 +78,17 @@ const result = await request(graphqlAPI, query);
 
 return result.posts;
 }
+
+export const getCategories = async () => {
+  const query = gql `
+    query GetCategories {
+      categories: {
+        name
+        slug
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+}
