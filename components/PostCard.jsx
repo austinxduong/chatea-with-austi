16ed5c028f38896c4fc31562296ae896bee51d8a
austinxduong <axduong2@gmail.com>
2021-12-01 17:59:03 -0800

    might change later styling

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -40,8 +40,8 @@ const PostCard = ({ post }) => (
       </p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-            <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-black text-lg font-medium border-b-2 border-pink-500 rounded-none text-white px-8 py-1 cursor-pointer">
-              <p className="hover:text-blue-200">Take a look</p>
+            <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-black text-lg font-medium border-b-2 border-blue-400 rounded-none text-white px-8 py-1 cursor-pointer">
+              <p className="hover:text-green-100">Take a look</p>
             </span>
         </Link>
       </div>
