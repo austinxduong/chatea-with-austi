2a8b17239100aaed3cf7033b69bf9a2b48110339
austinxduong <axduong2@gmail.com>
2021-12-03 00:29:53 -0800

    slug page, add globally

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
                 <div className="md:float-left block">
                 <Link href="/">
                     <span className="cursor-pointer text-4xl text-white">
-                        <h1 className="transition duration-700 text-center mb-8 font-arimo cursor-pointer hover:text-black">
+                        <h1 id="header" className="transition duration-700 text-center mb-8 font-arimo cursor-pointer hover:text-black">
                         Chatty, Chatea, with Austi
                         </h1>
                     </span>
