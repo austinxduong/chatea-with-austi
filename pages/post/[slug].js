c0a66232c6e2474fc4c61cb8b3ada02739615ede
austinxduong <axduong2@gmail.com>
2021-12-09 06:19:48 -0800

    hopefully this works

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 
 import { getPosts, getPostDetails } from '../../services';
-import { Header, PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+import { Header, PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
 
 const PostDetails = ({ post }) => {
+    const router = useRouter();
+
+    if(router.isFallback) {
+        <Loader />
+    }
 
     return (
         <div className="container mx-auto px-0 mb-8">
