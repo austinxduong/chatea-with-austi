ba31fb52686025e023ff429fb82797238b20a298
austinxduong <axduong2@gmail.com>
2021-12-09 15:50:59 -0800

    socials component

---
+++ components/Socials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Socials = () => {
+    return (
+        <div className="bg-white shadow-lg p-3 mb-8 animate-fade-in-up">
+            <h3 className="uppercase mb-2 border-b pb-b4">
+                Socials:
+            </h3>
+        </div>
+    )
+}
+
+export default Socials;
