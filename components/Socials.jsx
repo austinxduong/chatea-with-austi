56dd4c2aa98f25f9123e6d69f2a38cc8cd942bf1
austinxduong <axduong2@gmail.com>
2021-12-09 16:47:03 -0800

    socials

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -8,29 +8,61 @@ const Socials = () => {
                 Socials:
             </h3>
             <span className="cursor-pointer flex items-center flex-grow ml-4 transition duration-500 ease transform hover:-translate-y-1">
+                <img
+                    src="github.png"
+                    height="20px"
+                    width="20px"
+                    alt="Github"
+                    className="float-left"
+                    cite="https://www.flaticon.com/authors/hight-quality-icons"
+                    />
                 <Link href="https://github.com/austinxduong">
-                    <a className=" hover:text-pink-100 duration-400">
+                    <a className="ml-2 hover:text-pink-100 duration-400">
                         GitHub
                     </a>
                 </Link>
             </span>
             <span className="cursor-pointer flex items-center flex-grow ml-4 transition duration-500 ease transform hover:-translate-y-1">
+                <img
+                    src="linkedin.png"
+                    height="20px"
+                    width="20px"
+                    alt="LinkedIn"
+                    className="float-left"
+                    cite="https://www.flaticon.com/authors/najmunnahar"
+                />
                 <Link href="https://www.linkedin.com/in/austinxduong/">
-                    <a className="hover:text-pink-100 duration-400">
+                    <a className="ml-2 hover:text-pink-100 duration-400">
                         LinkedIn
                     </a>
                 </Link>
             </span>
             <span className="cursor-pointer flex items-center flex-grow ml-4 transition duration-500 ease transform hover:-translate-y-1">
+                <img
+                    src="facebook.png"
+                    height="20px"
+                    width="20px"
+                    alt="facebook"
+                    className="float-left"
+                    cite="https://www.flaticon.com/authors/najmunnahar"
+                    />
                 <Link href="https://www.facebook.com/axduongproductions/">
-                    <a className="hover:text-pink-100 duration-400">
+                    <a className="ml-2 hover:text-pink-100 duration-400">
                         Facebook
                     </a>
                 </Link>
             </span>
             <span className="cursor-pointer flex items-center flex-grow ml-4 transition duration-500 ease transform hover:-translate-y-1">
+                <img
+                    src="instagram.png"
+                    height="22px"
+                    width="22px"
+                    alt="instagram"
+                    className="float-left"
+                    cite="https://www.flaticon.com/authors/grow-studio"
+                    />
                 <Link href="https://www.instagram.com/austinxduong/?hl=en">
-                    <a className="hover:text-pink-100 duration-400">
+                    <a className="ml-2 hover:text-pink-100 duration-400">
                         Instagram
                     </a>
                 </Link>
