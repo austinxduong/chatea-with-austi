bdd9a687a3fe427c782e023885cd389a59f90a04
austinxduong <axduong2@gmail.com>
2021-12-10 12:15:18 -0800

    declare state addition

--- featured/FeaturedPosts.jsx
+++ featured/FeaturedPosts.jsx
@@ -4,3 +4,34 @@ import 'react-multi-carousel/lib/styles.css'
 
 import { FeaturedPostCard } from '../components'
 import { getFeaturedPosts } from '../services'
+
+const responsive = {
+    ultraScreenDesktop: {
+        breakpoint: { max: 4000, min: 1024 },
+        items: 5,
+    },
+    regulaDegulaDesktop: {
+        breakpoint: { max: 1024, min: 768 },
+        items: 3,
+    },
+    tablet: {
+        breakpoint: { max: 768, min: 640 },
+        items: 2,
+    },
+    mobile: {
+        breakpoint: { max: 640, min: 0 },
+    },
+};
+
+const FeaturedPosts = () => {
+    const [featuredPosts, setFeaturedPosts] = useState([]);
+    const [dataLoaded, setDataLoaded] = useState(false);
+
+    useEffect(() => {
+        getFeaturedPosts()
+            .then((result) => {
+                setFeaturedPosts(result);
+                setDataLoaded(true);
+            });
+    }, [])
+}
