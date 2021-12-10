97ca5ee5cc41b5faf208c7fd7693039e227a639a
austinxduong <axduong2@gmail.com>
2021-12-10 14:40:41 -0800

    refine

--- featured/FeaturedPosts.jsx
+++ featured/FeaturedPosts.jsx
@@ -52,7 +52,7 @@ const FeaturedPosts = () => {
   
     return (
       <div className="mb-8">
-        <Carousel infinite customLeftArrow={leftArrow} customRightArrow={rightArrow} responsive={responsive} itemClass="px-4">
+        <Carousel infinite customLeftArrow={leftArrow} customRightArrow={rightArrow} responsive={responsive} itemClass="px-4 animate-fade-in-down">
           {dataLoaded && featuredPosts.map((post, index) => (
             <FeaturedPostCard key={index} post={post} />
           ))}
